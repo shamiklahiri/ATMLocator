401cd31b0788725fffe61b7eeee81b97107d1217
shamiklahiri <shamiklahiri@gmail.com>
2019-10-16 21:15:19 +0200

    lean

--- src/main/java/com/backbase/atmlocator/service/ATMSearchEngine.java
+++ src/main/java/com/backbase/atmlocator/service/ATMSearchEngine.java
@@ -3,7 +3,6 @@ package com.backbase.atmlocator.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
